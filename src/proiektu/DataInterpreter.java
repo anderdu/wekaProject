5d8f6fe80f954bc1fec3e4fa214f2a315b28a36d
ander <anduran17f@gmail.com>
2020-03-14 17:09:40 +0100

    avanzando

--- src/proiektu/DataInterpreter.java
+++ src/proiektu/DataInterpreter.java
@@ -11,10 +11,12 @@ import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NominalToString;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 
 // C:\Users\andur\Programas\wekaData\Proiektua_text_mining\train.csv
 public class DataInterpreter {
+	private String procesedFilesPath = null; //Sortutzako fitxategiak hemen gordeko dira, parent forlder barruan
 	public static void main(String[] args) throws Exception {
 		DataInterpreter DI = new DataInterpreter();
 		String csvPath = args[0];
@@ -27,7 +29,45 @@ public class DataInterpreter {
 		//3. StringToWord filtroa aplikatu fitxategiaren textuak hitzetan banantzeko
 		File wordArff = DI.toWordVector(newArff);
 		
-		
+	}
+	
+	public File fileParser(String originalCSV) {
+		/* CSV fitxategia prosezatzen du konfliktorik ez egoteko wekak egiten duen interpretazioarekin
+		 * in: original csv
+		 * out: parsed csv in parent folder
+		 * 	  return parsed csv
+		 */
+		File parsedCSV=null;
+		//fitxategia sortu
+		try {
+			File origFile = new File(originalCSV);
+			this.procesedFilesPath = origFile.getParent()+"\\procesedFiles";
+			File parent = new File(procesedFilesPath);
+			if(!parent.isDirectory()) parent.mkdir(); //direktorio ez bada existitzen, horain sortuko du
+			String name = origFile.getName().split("\\.")[0]; //fitxeroaren izena lortzen du, parent barruan
+			String newFileName = procesedFilesPath+"\\"+name+"Parsed.csv"; //Sortuko dugun CSV berriaren izena definitu
+			parsedCSV = new File(newFileName);
+			parsedCSV.createNewFile(); //Fisikoki sortu
+			
+			//filewriter prestatu
+			FileWriter writer = new FileWriter(parsedCSV);
+
+			//fitxategia prozezatu
+			BufferedReader csvReader = new BufferedReader(new FileReader(origFile));
+			String line;
+				while (true) {
+					line = csvReader.readLine();
+					if(line==null) break;
+				    line = line.replace("'", "`");
+				    //line = line.replace("?", "_?");
+				    writer.append(line+"\n");
+				    writer.flush();
+				}
+			csvReader.close();
+		} catch (Exception e) {
+			System.out.println("fileParser - error");
+		}
+		return parsedCSV;
 	}
 	
 	public File csvToArrf(File parsedCSV) throws IOException {
@@ -41,82 +81,74 @@ public class DataInterpreter {
 		
 //	    loadeeer.setSource(otro);
 		// load CSV
-
 		try {
 		    CSVLoader loader = new CSVLoader();
 		    loader.setSource(parsedCSV);
 		    data = loader.getDataSet();
 		} catch (Exception e) {
-			System.out.println("exception loading");
+			System.out.println("csvToArrf- exception loading");
 		}
 
+		//arrf filtroa aplikatu nominalToString
+		try {
+			NominalToString filterString = new NominalToString();
+			filterString.setAttributeIndexes("2");
+			filterString.setInputFormat(data);
+			data = Filter.useFilter(data, filterString);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 
-
-	    // save ARFF
-		File newArrf = new File(csvPath.replace(".csv", ".arff"));
+	    // save ARFF  -  Fitxategia sortu
+		File newArrf = new File(csvPath.replace(".csv", ".arff")); //csv-aren izen eta kokapen bera, baina .arrf
 	    try {
 		    ArffSaver saver = new ArffSaver();
 		    saver.setInstances(data);
 		    saver.setFile(newArrf);
 		    saver.writeBatch();
 	    }catch (Exception e) {
-	    	System.out.println("saver exception");
+	    	System.out.println("csvToArrf - saver exception");
 		}
 	    return newArrf;
 	}
 	
-	public File fileParser(String originalCSV) {
-		/* CSV fitxategia prosezatzen du konfliktorik ez egoteko wekak egiten duen interpretazioarekin
-		 * in: original csv
-		 * out: parsed csv in parent folder
-		 * 	return parsed csv
+	public File toWordVector(File originalArff) {
+		/* Lehen sortutako arrf fitxategiaren textuak hitzetan banatzen du
+		 * in:  originalArff
+		 * out: words.arff in parent folder
+		 * 	  return wordsArff
 		 */
-		File parsedCSV=null;
-		try {
-			//fitxategia sortu
-			File origFile = new File(originalCSV);
-			String parent = origFile.getParent();
-			String name = origFile.getName().split("\\.")[0];
-			String newFileName = parent+"\\"+name+"Parsed.csv";
-			parsedCSV = new File(newFileName);
-			parsedCSV.createNewFile();
-			
-			//filewriter prestatu
-			FileWriter writer = new FileWriter(parsedCSV);
-
-			//fitxategia prozezatu
-			BufferedReader csvReader = new BufferedReader(new FileReader(origFile));
-			String line;
-				while (true) {
-					line = csvReader.readLine();
-					if(line==null) break;
-//				    System.out.println(Arrays.toString(data));
-				    line = line.replace("'", "`");
-				    //line = line.replace("?", "_?");
-				    writer.append(line+"\n");
-				    writer.flush();
-				    //pSearchAlgorithm = algorithm.getClass().getName().split("\\.")[3]
-				}
-			csvReader.close();
-		} catch (Exception e) {
-			System.out.println("fileparser exception");
-		}
-		return parsedCSV;
-	}
-	
-	public File toWordVector(File originalArff) throws Exception {
 		//arrf-a kargatu
-		DataSource source = new DataSource(originalArff.getAbsolutePath());
-		Instances data = source.getDataSet();
+		Instances data=null;
+		try {
+			DataSource source = new DataSource(originalArff.getAbsolutePath());
+			data = source.getDataSet();
+		} catch (Exception e) {
+			System.out.println("toWordVector - arrf karga error");
+		}
 		
-		//filtroa aplikatu
-		StringToWordVector filter = new StringToWordVector();
-		filter.setInputFormat(data);
-		Instances newData = Filter.useFilter(data, filter);
+		//filtroa aplikatu - String-ak hitzetan banantzeko
+		Instances wordsData=null;
+		try {
+			StringToWordVector filter = new StringToWordVector();
+			filter.setInputFormat(data);
+			wordsData = Filter.useFilter(data, filter);
+		} catch (Exception e) {
+			System.out.println("toWordVector - filter error");
+		}
 		
+		//Fitxategia sortu, words.arrf, filtroa aplikatuta dago
+		File wordsArff = new File(procesedFilesPath+"\\words.arff");
+		try {
+		    ArffSaver saver = new ArffSaver();
+		    saver.setInstances(wordsData);
+		    saver.setFile(wordsArff);
+		    saver.writeBatch();
+		} catch (Exception e) {
+			System.out.println("toWordVector - file creator error");
+		}
 		
-		return originalArff;
-		
+		return wordsArff;
 	}
 
 }
